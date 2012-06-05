f940093ed05eef0070273fae39d34bf056811441
Chris Maddox <tyre77@gmail.com>
2012-06-05 16:50:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Persistence::Application.config.secret_token = 'b4be0fe2648e2fc5a18657d30aada130e1f141ee5dbfd6642a041b49e1808fc7d0847d4708a8663e4170df9f5fecf4c90fa6169beaaf8189c06e851236aaeaa6'
