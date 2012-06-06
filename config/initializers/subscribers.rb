e77218046295394707c502d08510d305522ad175
Chris Maddox <tyre77@gmail.com>
2012-06-06 13:58:56 -0400

    create and destroy fixed to check for existence of passed in json keys on model

--- config/initializers/subscribers.rb
+++ config/initializers/subscribers.rb
@@ -1,9 +1,19 @@
+
 Thread.new do
   update_subscriber = Subscriber.new('update') do |parsed_json, type|
     model = type.capitalize.constantize
-    if instance = model.find_by_id(parsed_json[type]['id'])
+    if instance = model.find(parsed_json[type])
       parsed_json[type].delete('id')
       instance.update_attributes parsed_json[type]
+    end
+  end
+end
+
+Thread.new do
+  create_subscriber = Subscriber.new('create') do |parsed_json, type|
+    model = type.capitalize.constantize
+    if parsed_json[type].keys.all? { |key| model.accessible_attributes.include?(key) }
+      model.create(parsed_json[type])
     end
   end
 end
@@ -12,16 +22,9 @@ Thread.new do
   destroy_subscriber = Subscriber.new('destroy') do |parsed_json, type|
     if type
       model = type.capitalize.constantize
-      if instance = model.find_by_id(parsed_json[type]['id'])
+      if instance = model.where(parsed_json[type]).first
         instance.destroy
       end
     end
-  end
-end
-
-Thread.new do
-  create_subscriber = Subscriber.new('create') do |parsed_json, type|
-    model = type.capitalize.constantize
-    model.create(parsed_json[type])
   end
 end
