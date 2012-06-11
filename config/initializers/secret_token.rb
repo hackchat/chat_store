964a4f4b32a5b93df37f071a1d877d8adbde1fdb
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 10:24:11 -0400

    more changes

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Persistence::Application.config.secret_token = 'b4be0fe2648e2fc5a18657d30aada130e1f141ee5dbfd6642a041b49e1808fc7d0847d4708a8663e4170df9f5fecf4c90fa6169beaaf8189c06e851236aaeaa6'
+Persistence::Application.config.secret_token = '0dce1341585a60f07b1756ef3a9b6a47efcd29a4635e4e9150f18f9925d2a50d847323055e3d94e67193d28f1a29363057785d6afbab66985909b17b567e25b6'
