9b203c5df167d78b970eb9c2a6694d3ee775e1c0
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 11:12:20 -0400

    changed use sudo to true and reverted pty to true

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ set :application, "chat_store"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-set :use_sudo, false
+set :use_sudo, true
 
 set :scm, "git"
 set :repository, "git@github.com:hackchat/#{application}.git"
 set :branch, "master"
 
-default_run_options[:pty] = false
+default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
