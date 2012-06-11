ac0521ec185351f9e31580ca4f7c2041b85598ab
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 11:07:31 -0400

    from run to sudo

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
+      sudo "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
