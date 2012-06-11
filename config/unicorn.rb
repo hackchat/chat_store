d44f429a9161ac7ecb22f4eeaa5b570439343f47
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 09:55:23 -0400

    removed public from unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/deployer/apps/chat_store/current/public"
+root = "/home/deployer/apps/chat_store/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
