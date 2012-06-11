951ce7042e29186b0e63ec13a913077b727432f5
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 10:11:07 -0400

    one more line removed from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,6 @@ namespace :deploy do
   end
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     puts "Now edit the config files in #{shared_path}."
