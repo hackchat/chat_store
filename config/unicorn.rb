e1bce66d4454c425d299c2814d3e28a33a6f3561
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-11 10:42:55 -0400

    more

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 root = "/home/deployer/apps/chat_store/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn_chat_store.log"
+stdout_path "#{root}/log/unicorn_chat_store.log"
 
 listen "/tmp/unicorn.chat_store.sock"
 listen 7002
